33f7c779b7ddab2b85b3077f95b6d87a56f31727
billouboq <aurelien.boquet.web@gmail.com>
2020-03-12 11:55:53 +0100

    remove deprecated ViewPropTypes

--- src/pagination/Pagination.js
+++ src/pagination/Pagination.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
-import { I18nManager, Platform, View, ViewPropTypes } from 'react-native';
-import PropTypes from 'prop-types';
+import { I18nManager, Platform, View } from 'react-native';
 import PaginationDot from './PaginationDot';
 import styles from './Pagination.style';
 
@@ -8,146 +7,143 @@ const IS_IOS = Platform.OS === 'ios';
 const IS_RTL = I18nManager.isRTL;
 
 export default class Pagination extends PureComponent {
+	static defaultProps = {
+		inactiveDotOpacity: 0.5,
+		inactiveDotScale: 0.5,
+		tappableDots: false,
+		vertical: false
+	};
 
-    static propTypes = {
-        activeDotIndex: PropTypes.number.isRequired,
-        dotsLength: PropTypes.number.isRequired,
-        activeOpacity: PropTypes.number,
-        carouselRef: PropTypes.object,
-        containerStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
-        dotColor: PropTypes.string,
-        dotContainerStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
-        dotElement: PropTypes.element,
-        dotStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
-        inactiveDotColor: PropTypes.string,
-        inactiveDotElement: PropTypes.element,
-        inactiveDotOpacity: PropTypes.number,
-        inactiveDotScale: PropTypes.number,
-        inactiveDotStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
-        renderDots: PropTypes.func,
-        tappableDots: PropTypes.bool,
-        vertical: PropTypes.bool,
-        accessibilityLabel: PropTypes.string
-    };
+	constructor(props) {
+		super(props);
 
-    static defaultProps = {
-        inactiveDotOpacity: 0.5,
-        inactiveDotScale: 0.5,
-        tappableDots: false,
-        vertical: false
-    }
+		// Warnings
+		if (
+			(props.dotColor && !props.inactiveDotColor) ||
+			(!props.dotColor && props.inactiveDotColor)
+		) {
+			console.warn(
+				'react-native-snap-carousel | Pagination: ' +
+					'You need to specify both `dotColor` and `inactiveDotColor`'
+			);
+		}
+		if (
+			(props.dotElement && !props.inactiveDotElement) ||
+			(!props.dotElement && props.inactiveDotElement)
+		) {
+			console.warn(
+				'react-native-snap-carousel | Pagination: ' +
+					'You need to specify both `dotElement` and `inactiveDotElement`'
+			);
+		}
+		if (props.tappableDots && !props.carouselRef) {
+			console.warn(
+				'react-native-snap-carousel | Pagination: ' +
+					'You must specify prop `carouselRef` when setting `tappableDots` to `true`'
+			);
+		}
+	}
 
-    constructor (props) {
-        super(props);
+	_needsRTLAdaptations() {
+		const { vertical } = this.props;
+		return IS_RTL && !IS_IOS && !vertical;
+	}
 
-        // Warnings
-        if ((props.dotColor && !props.inactiveDotColor) || (!props.dotColor && props.inactiveDotColor)) {
-            console.warn(
-                'react-native-snap-carousel | Pagination: ' +
-                'You need to specify both `dotColor` and `inactiveDotColor`'
-            );
-        }
-        if ((props.dotElement && !props.inactiveDotElement) || (!props.dotElement && props.inactiveDotElement)) {
-            console.warn(
-                'react-native-snap-carousel | Pagination: ' +
-                'You need to specify both `dotElement` and `inactiveDotElement`'
-            );
-        }
-        if (props.tappableDots && !props.carouselRef) {
-            console.warn(
-                'react-native-snap-carousel | Pagination: ' +
-                'You must specify prop `carouselRef` when setting `tappableDots` to `true`'
-            );
-        }
-    }
+	get _activeDotIndex() {
+		const { activeDotIndex, dotsLength } = this.props;
+		return this._needsRTLAdaptations()
+			? dotsLength - activeDotIndex - 1
+			: activeDotIndex;
+	}
 
-    _needsRTLAdaptations () {
-        const { vertical } = this.props;
-        return IS_RTL && !IS_IOS && !vertical;
-    }
+	get dots() {
+		const {
+			activeOpacity,
+			carouselRef,
+			dotsLength,
+			dotColor,
+			dotContainerStyle,
+			dotElement,
+			dotStyle,
+			inactiveDotColor,
+			inactiveDotElement,
+			inactiveDotOpacity,
+			inactiveDotScale,
+			inactiveDotStyle,
+			renderDots,
+			tappableDots
+		} = this.props;
 
-    get _activeDotIndex () {
-        const { activeDotIndex, dotsLength } = this.props;
-        return this._needsRTLAdaptations() ? dotsLength - activeDotIndex - 1 : activeDotIndex;
-    }
+		if (renderDots) {
+			return renderDots(this._activeDotIndex, dotsLength, this);
+		}
 
-    get dots () {
-        const {
-            activeOpacity,
-            carouselRef,
-            dotsLength,
-            dotColor,
-            dotContainerStyle,
-            dotElement,
-            dotStyle,
-            inactiveDotColor,
-            inactiveDotElement,
-            inactiveDotOpacity,
-            inactiveDotScale,
-            inactiveDotStyle,
-            renderDots,
-            tappableDots
-        } = this.props;
+		const DefaultDot = (
+			<PaginationDot
+				carouselRef={carouselRef}
+				tappable={tappableDots && typeof carouselRef !== 'undefined'}
+				activeOpacity={activeOpacity}
+				color={dotColor}
+				containerStyle={dotContainerStyle}
+				style={dotStyle}
+				inactiveColor={inactiveDotColor}
+				inactiveOpacity={inactiveDotOpacity}
+				inactiveScale={inactiveDotScale}
+				inactiveStyle={inactiveDotStyle}
+			/>
+		);
 
-        if (renderDots) {
-            return renderDots(this._activeDotIndex, dotsLength, this);
-        }
+		let dots = [];
 
-        const DefaultDot = <PaginationDot
-          carouselRef={carouselRef}
-          tappable={tappableDots && typeof carouselRef !== 'undefined'}
-          activeOpacity={activeOpacity}
-          color={dotColor}
-          containerStyle={dotContainerStyle}
-          style={dotStyle}
-          inactiveColor={inactiveDotColor}
-          inactiveOpacity={inactiveDotOpacity}
-          inactiveScale={inactiveDotScale}
-          inactiveStyle={inactiveDotStyle}
-        />;
+		for (let i = 0; i < dotsLength; i++) {
+			const isActive = i === this._activeDotIndex;
+			dots.push(
+				React.cloneElement(
+					(isActive ? dotElement : inactiveDotElement) || DefaultDot,
+					{
+						key: `pagination-dot-${i}`,
+						active: i === this._activeDotIndex,
+						index: i
+					}
+				)
+			);
+		}
 
-        let dots = [];
+		return dots;
+	}
 
-        for (let i = 0; i < dotsLength; i++) {
-            const isActive = i === this._activeDotIndex;
-            dots.push(React.cloneElement(
-                (isActive ? dotElement : inactiveDotElement) || DefaultDot,
-                {
-                    key: `pagination-dot-${i}`,
-                    active: i === this._activeDotIndex,
-                    index: i
-                }
-            ));
-        }
+	render() {
+		const {
+			dotsLength,
+			containerStyle,
+			vertical,
+			accessibilityLabel
+		} = this.props;
 
-        return dots;
-    }
+		if (!dotsLength || dotsLength < 2) {
+			return false;
+		}
 
-    render () {
-        const { dotsLength, containerStyle, vertical, accessibilityLabel } = this.props;
+		const style = [
+			styles.sliderPagination,
+			{
+				flexDirection: vertical
+					? 'column'
+					: this._needsRTLAdaptations()
+					? 'row-reverse'
+					: 'row'
+			},
+			containerStyle || {}
+		];
 
-        if (!dotsLength || dotsLength < 2) {
-            return false;
-        }
-
-        const style = [
-            styles.sliderPagination,
-            { flexDirection: vertical ?
-                'column' :
-                (this._needsRTLAdaptations() ? 'row-reverse' : 'row')
-            },
-            containerStyle || {}
-        ];
-
-        return (
-            <View
-              pointerEvents={'box-none'}
-              style={style}
-              accessible={!!accessibilityLabel}
-              accessibilityLabel={accessibilityLabel}
-            >
-                { this.dots }
-            </View>
-        );
-    }
+		return (
+			<View
+				pointerEvents={'box-none'}
+				style={style}
+				accessible={!!accessibilityLabel}
+				accessibilityLabel={accessibilityLabel}>
+				{this.dots}
+			</View>
+		);
+	}
 }
